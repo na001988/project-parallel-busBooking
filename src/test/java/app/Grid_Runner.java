fc2d1ae08722beb1e84a45902f75b8bfe91455c9
Roberto Zumba <onlyblue@outlook.com.au>
2022-12-06 17:26:33 +1100

    update grid parameters to 7 machines

--- src/test/java/app/Grid_Runner.java
+++ src/test/java/app/Grid_Runner.java
@@ -12,54 +12,57 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 
-public class Runner {
+public class Grid_Runner {
 	
-    WebDriver driver_1;
+	WebDriver driver_1;
     WebDriver driver_2;
     String browser_c ="chrome";
     String browser_f ="firefox";
     String path_browser_f ="C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+    String client = "http://192.168.0.4:4444/wd/hub";
     
     String url = "https://www.google.com.au/";
 
     
   @BeforeClass(groups="start")
+ 
   public void run1() throws InterruptedException{
+	  
 	  try {
 	  System.out.println("@@@@ Set-up browser <"+browser_c+"> for automation");
 	  
+	  	ChromeOptions c = new ChromeOptions();
+		c.setCapability("browserName", browser_c);
+		c.setCapability("Platform", "WIN10");
+		c.setHeadless(true);
 	  
-	  ChromeOptions op = new ChromeOptions();
-		op.setCapability("browserName", browser_c);
-		op.setCapability("Platform", "WIN10");
-		op.setHeadless(true);
-	  
-	  URL uri = new URL("http://192.168.0.4:4444/wd/hub");
-		driver_2 = new RemoteWebDriver(uri,op);
+	    URL uri = new URL(client);
+		driver_1 = new RemoteWebDriver(uri,c);
 	  
 	  
 	  }catch(Exception e) {
-			System.out.println("On-Run1 > Error: "+e);
+			System.out.println("Object-Run1 > Error: "+e);
 		}
+	  
 	  
 	  
 	  try {
 	  System.out.println("@@@@ Set-up browser <"+browser_f+"> for automation");
 	  
-	  FirefoxOptions op = new FirefoxOptions();
-		op.setCapability("browserName", browser_f);
-		op.setCapability("Platform", "WIN10");
-		op.setBinary(path_browser_f);
-		op.setHeadless(true);
+	  FirefoxOptions f = new FirefoxOptions();
+		f.setCapability("browserName", browser_f);
+		f.setCapability("Platform", "WIN10");
+		f.setBinary(path_browser_f);
+		f.setHeadless(true);
 	  
-	  URL uri = new URL("http://192.168.0.4:4444/wd/hub");
-		driver_1 = new RemoteWebDriver(uri,op);
+		URL uri = new URL(client);
+		driver_2 = new RemoteWebDriver(uri,f);
 	  
 		
 	  }catch(Exception e) {
-			System.out.println("On-run2 > Error: "+e);
+			System.out.println("Object-run2 > Error: "+e);
 		}
-		
+	  
   }
   
   @AfterClass(groups="end")
@@ -79,35 +82,25 @@ public class Runner {
 	  System.out.println("Web Browser "+browser_c+ " started...." + Thread.currentThread().getId());
 	  driver_2.get(url);
 	  Thread.sleep(1000);
+	  System.out.println(driver_2.getTitle());
 	  }catch(Exception e) {
 		  System.out.println("Warning...... while launching: "+browser_c+" | "+e.getMessage());
 	  }
 	  
   }
   
-  @Test(groups="chrome", dependsOnMethods="launch_chrome")
-  public void launch_Perform_App1() {
-	  System.out.println("Reading elements on browser "+browser_c+"  !!!!! > " + Thread.currentThread().getId());
-  }
-  
-  
-
   @Test(groups="firefox")
   public void launch_Firefox() throws InterruptedException {
 	  try {
 		  System.out.println("Web Browser "+browser_f+ " started...." + Thread.currentThread().getId());
 	  driver_1.get(url);
 	  Thread.sleep(1500);
+	  System.out.println(driver_1.getTitle());
 	  }catch(Exception e) {
 		  System.out.println("Warning...... while launching: "+browser_f+" | "+e.getMessage());
 	  }
 	  
   }
-  
-  @Test(groups="Firefox", dependsOnMethods="launch_Firefox")
-  public void launch_Perform_App2() {
-	  System.out.println("Reading elements on browser "+browser_f+"  !!!!! > " + Thread.currentThread().getId());
-  }
-  
 
+  
 }
